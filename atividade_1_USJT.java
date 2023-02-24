4f368f7b50257621faf72361653e5a237d5db873
maduas <102772880+maduas@users.noreply.github.com>
2023-02-24 20:27:50 -0300

    Add files via upload

---
+++ atividade_1_USJT.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class atividade_1_USJT{
+
+    public static void main(String[] args) {
+      /* Implemente um programa para cadastro de alunos de uma disciplina utilizando a linguagem Java, seguindo os seguintes critérios: 
+       Criar uma estrutura heterogênea (array) do tipo string para cadastrar o nome de 25 alunos; Garantir que a estrutura seja totalmente
+        preenchida. Após implementar o programa e preencher o array com o nome dos alunos, cadastre mais 5 novos alunos. 
+        Qual a dificuldade de inserir os novos alunos nessa estrutura de dados? Justifique a sua resposta direcionando aos 
+        conceitos de gerenciamento de memória. */
+      
+      //Scanner s = new Scanner(System.in);
+      String Cadastro_Alunos[] = new String[25];
+      System.out.println("Cadastre um aluno na base:");
+      System.out.println(Cadastro_Alunos[0] = "Luisa");
+      System.out.println(Cadastro_Alunos[1] = "Luis");
+      System.out.println(Cadastro_Alunos[2] = "Maria Luisa");
+      System.out.println(Cadastro_Alunos[3] = "Robson");
+      System.out.println(Cadastro_Alunos[4] = "Mario");
+      System.out.println(Cadastro_Alunos[5] = "Jão");
+      System.out.println(Cadastro_Alunos[6] = "Mário");
+      System.out.println(Cadastro_Alunos[7] = "João Pedro");
+      System.out.println(Cadastro_Alunos[8] = "Lucas");
+      System.out.println(Cadastro_Alunos[9] = "Natalia");
+      System.out.println(Cadastro_Alunos[10] = "Maria");
+      System.out.println(Cadastro_Alunos[11] = "Eduarda");
+      System.out.println(Cadastro_Alunos[12] = "Clara");
+      System.out.println(Cadastro_Alunos[13] = "Ana");
+      System.out.println(Cadastro_Alunos[14] = "Carolina");
+      System.out.println(Cadastro_Alunos[15] = "Ester");
+      System.out.println(Cadastro_Alunos[16] = "Izabel");
+      System.out.println(Cadastro_Alunos[17] = "Raquel");
+      System.out.println(Cadastro_Alunos[18] = "Matheus");
+      System.out.println(Cadastro_Alunos[19] = "Pedro");
+      System.out.println(Cadastro_Alunos[20] = "Deivson");
+      System.out.println(Cadastro_Alunos[21] = "Felipe");
+      System.out.println(Cadastro_Alunos[21] = "Gabriel");
+      System.out.println(Cadastro_Alunos[22] = "Gabriela");
+      System.out.println(Cadastro_Alunos[23] = "Márcia");
+      System.out.println(Cadastro_Alunos[24] = "Eduardo");
+      System.out.println(Cadastro_Alunos[25] = "ALUNO 1");
+      System.out.println(Cadastro_Alunos[26] = "ALUNO 2");
+      System.out.println(Cadastro_Alunos[27] = "ALUNO 3");
+      System.out.println(Cadastro_Alunos[28] = "ALUNO 4");
+      System.out.println(Cadastro_Alunos[29] = "ALUNO 5");
+
+    }
+ }
